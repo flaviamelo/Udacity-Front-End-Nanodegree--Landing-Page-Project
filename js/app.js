d66411668be2b7977e25fbe5101d33e1112559a2
Flavia Melo Barreto <flavia.melo@live.com>
2020-10-14 18:26:21 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -25,17 +25,51 @@ function buildNav() {
   });
   myNav.appendChild(fragment);
 };
-
 buildNav();
 
 
-//Active Page Link Button Highlight
+/*//Active Page Link Button Highlight
 $(document).ready(function(){
   $('ul li a').click(function(){
     $('li a').removeClass("active");
     $(this).addClass("active");
   })
-})
+})*/
+
+
+// Active States
+const offset = (section) => {
+  return Math.floor(section.getBoundingClientRect().top);
+};
+const clearActive = (section) => {
+  section.classList.remove('your-active-class');
+  section.classList.remove('focus');
+};
+const insertActive = (conditional, section) => {
+  if (conditional) {
+      section.classList.add('your-active-class');
+      section.classList.add('focus'); 
+  };
+};
+const activeSection = () => {
+  mySections.forEach(section => {
+      const elementOffset = offset(section);
+
+      inviewport = () => elementOffset < 180 && elementOffset >= -180;
+
+      clearActive(section);
+      insertActive(inviewport(), section);
+
+      $(document).ready(function(section){
+        $('ul li a').click(function(){
+          $('li a').removeClass("active");
+          $(this).addClass("active");
+        })
+      })
+
+  });
+};
+window.addEventListener('scroll', activeSection);
 
 
 //Slideshow
@@ -53,3 +87,26 @@ function slideShow() {
   x[myIndex-1].style.display = "block";  
   setTimeout(slideShow, 2000); // Change image every 2 seconds
 }
+
+
+//Top Button
+const goTop = document.getElementById("topButton");
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goTop.style.display = "block";
+    } else {
+        goTop.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+
